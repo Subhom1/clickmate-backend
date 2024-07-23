9178e6572b415959a1d3c0c1e271ce1ca074a2c3
Subhom Kundu <subhomkundu@gmail.com>
2024-07-23 22:36:10 +0100

    fetch user by email and user schema

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserDetailSchema from "./models/UserDetailSchema.js";
 import InterestSchema from "./models/InterestSchema.js";
+import admin from "./firebaseAdmin.js";
 
 const app = express();
 const port = 5051;
@@ -17,6 +18,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
+//To fetch all the users
 app.get("/users", async (req, res) => {
   try {
     const users = await UserDetailSchema.find();
@@ -25,6 +27,7 @@ app.get("/users", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+// To register a user
 const User = mongoose.model("user");
 app.post("/register", async (req, res) => {
   const { name, email } = req.body;
@@ -33,7 +36,7 @@ app.post("/register", async (req, res) => {
   if (oldUser) return res.status(409).send({ data: "User already exists!" });
   try {
     await User.create({
-      username: name,
+      fullname: name,
       email,
       created_at: Date(),
       updated_at: Date(),
@@ -45,36 +48,10 @@ app.post("/register", async (req, res) => {
     res.status(500).send({ status: "error", data: err.res });
   }
 });
-const Interest = mongoose.model("interest");
-// Endpoint to add interests
-app.post("/interests", async (req, res) => {
-  console.log(req.body);
-  const { interest } = req.body;
-  try {
-    const existingInterestDoc = await Interest.findOne();
-    if (!existingInterestDoc) {
-      await Interest.create({ interest });
-      return res
-        .status(201)
-        .send({ status: "ok", data: "Interests added successfully" });
-    }
-    const updatedInterests = Array.from(
-      new Set([...existingInterestDoc.interest, ...interest])
-    );
-    existingInterestDoc.interest = updatedInterests;
-    await existingInterestDoc.save();
-    res
-      .status(201)
-      .send({ status: "ok", data: "Interests updated successfully" });
-  } catch (err) {
-    res.status(500).send({ status: "error", data: err.message });
-  }
-});
 // Endpoint to add interests to an existing user
 app.post("/user/:id/interests", async (req, res) => {
   const { id } = req.params;
-  const { interest } = req.body; // Expecting an array of objects with id
-
+  const { interest } = req.body; // Expecting an array of objects with id "interest": [{ "id": "669d98bae0b0218423887373" }]
   // Validate request
   if (
     !Array.isArray(interest) ||
@@ -85,7 +62,6 @@ app.post("/user/:id/interests", async (req, res) => {
       .status(400)
       .send({ status: "error", message: "Invalid interest format" });
   }
-
   try {
     // Extract the IDs from the request payload
     const interestIds = interest.map((item) => item.id);
@@ -109,7 +85,71 @@ app.post("/user/:id/interests", async (req, res) => {
     res.status(500).send({ status: "error", message: err.message });
   }
 });
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+// Fetch user's details by useremail
+app.get("/user/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .send({ status: "error", message: "User not found" });
+    }
+    res.status(200).send({ status: "ok", data: user });
+  } catch (err) {
+    res.status(500).send({ status: "error", data: err.message });
+  }
+});
+//Delete user
+app.delete("/deleteUser/:uid/:fuid", async (req, res) => {
+  const { uid, fuid } = req.params;
+
+  try {
+    // Delete user from MongoDB using Mongoose
+    const result = await User.deleteOne({ _id: uid });
+    // await admin.auth().deleteUser(fuid);
+
+    if (result.deletedCount === 1) {
+      // Delete user from Firebase Auth
+      res.status(200).send({
+        message: "User deleted successfully from Firebase and MongoDB",
+      });
+    } else {
+      res.status(404).send({ message: "User not found in MongoDB" });
+    }
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res
+      .status(500)
+      .send({ message: "Error deleting user", error: error.message });
+  }
+});
+
+// const Interest = mongoose.model("interest");
+// Endpoint to add interests //ONLY AS A ADMIN IF I WANT TO ADD ANY NEW INTEREST TO THE DB
+// app.post("/interests", async (req, res) => {
+//   console.log(req.body);
+//   const { interest } = req.body;
+//   try {
+//     const existingInterestDoc = await Interest.findOne();
+//     if (!existingInterestDoc) {
+//       await Interest.create({ interest });
+//       return res
+//         .status(201)
+//         .send({ status: "ok", data: "Interests added successfully" });
+//     }
+//     const updatedInterests = Array.from(
+//       new Set([...existingInterestDoc.interest, ...interest])
+//     );
+//     existingInterestDoc.interest = updatedInterests;
+//     await existingInterestDoc.save();
+//     res
+//       .status(201)
+//       .send({ status: "ok", data: "Interests updated successfully" });
+//   } catch (err) {
+//     res.status(500).send({ status: "error", data: err.message });
+//   }
+// });
--- models/UserDetailSchema.js
+++ models/UserDetailSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserDetailSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     interest: [{ type: mongoose.Schema.Types.ObjectId, ref: "interest" }],
     created_at: { type: Date, default: Date() },
