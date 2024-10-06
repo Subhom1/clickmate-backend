e8ada3dadb5eaab8b2de446d370d4cf99eaa9411
Subhom Kundu <subhomkundu@gmail.com>
2024-10-06 11:45:28 +0100

    added firebase admin file

---
+++ FirebaseAdmin.js
@@ -0,0 +1,17 @@
+import admin from "firebase-admin";
+import { readFile } from "fs/promises";
+
+const serviceAccount = JSON.parse(
+  await readFile(
+    new URL(
+      "./clickmate-57ede-firebase-adminsdk-7qhtz-41c63795e9.json",
+      import.meta.url
+    )
+  )
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
